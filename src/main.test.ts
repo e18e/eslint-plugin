ed644ce8f8f2491947fa1edd047ca8a044841cd1
Dom Porada <dom@dom.engineering>
2026-03-28 20:10:02 +0100

    Tighten types on exported configs

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest';
-import {ESLint, type Linter} from 'eslint';
+import {ESLint} from 'eslint';
 import * as jsoncParser from 'jsonc-eslint-parser';
 import json from '@eslint/json';
 import plugin from './main.js';
@@ -15,7 +15,7 @@ describe('main plugin', () => {
             parser: jsoncParser
           }
         },
-        plugin.configs!.recommended as Linter.Config
+        plugin.configs.recommended
       ]
     });
 
@@ -41,7 +41,7 @@ describe('main plugin', () => {
           },
           language: 'json/json'
         },
-        plugin.configs!.recommended as Linter.Config
+        plugin.configs.recommended
       ]
     });
 
